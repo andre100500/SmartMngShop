d18f83568125089ae658c5fbd5ecc8f5da598a17
andre100500 <andre100500@gmail.com>
2024-10-22 01:10:53 +0300

    Customer model

--- SmartMngShop.Customer/Domain/Models/DateCustomerWork.cs
+++ SmartMngShop.Customer/Domain/Models/DateCustomerWork.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string JobTitle { get; set; }
-        public DateOnly DateTimeStartPrgoress { get; set; }
+        public required DateOnly DateTimeStartPrgoress { get; set; }
         public DateOnly? DateTimeStopPrgoress { get; set; } = null;
     }
 }
