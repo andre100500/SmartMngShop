ad3c547f11fe29d56a37fc1d81b00a08652df7c3
andre100500 <andre100500@gmail.com>
2024-10-31 14:35:32 +0200

    fix

--- SmartMngShop.Customer/Domain/Models/DateCustomerWork.cs
+++ SmartMngShop.Customer/Domain/Models/DateCustomerWork.cs
@@ -10,13 +10,33 @@
 
         public int Vacation { get; set; }
 
-        private List<DateCustomerWork> CalculateVacation( int startVacation, int daysSinceStop, List<DateCustomerWork> customerWork)
+        private List<DateCustomerWork> CalculateVacation( int startVacation, int daysSinceStop, List<DateCustomerWork> customerWork, int minWorkingDays)
         {
             int vacationThreshold = startVacation;
-     
+            DateTime today = DateTime.UtcNow;
+
             var usersWithVacationLeft = customerWork
-                .Where(x => x.Vacation > vacationThreshold && x.Vacation >= daysSinceStop)
-                .ToList();
+        .Select(x =>
+        {
+            
+            var daysWorked = (today - x.DateTimeStartPrgoress.ToDateTime(TimeOnly.MinValue)).Days;
+
+           
+            var vacationDays = daysWorked >= 28 ? (daysWorked / 28) * 5 : 0;
+
+           
+            return new DateCustomerWork
+            {
+                Id = x.Id,
+                Name = x.Name,
+                JobTitle = x.JobTitle,
+                DateTimeStartPrgoress = x.DateTimeStartPrgoress,
+                DateTimeStopPrgoress = x.DateTimeStopPrgoress,
+                Vacation = vacationDays
+            };
+        })
+        .Where(x => x.Vacation > startVacation)
+        .ToList();
 
             return usersWithVacationLeft;
         }
