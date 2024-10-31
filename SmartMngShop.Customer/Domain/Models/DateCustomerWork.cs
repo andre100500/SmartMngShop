fe3efcd3a042898562a9bfc5706bcec5e57f3e4c
andre100500 <andre100500@gmail.com>
2024-10-31 15:01:51 +0200

    add async Task for method

--- SmartMngShop.Customer/Domain/Models/DateCustomerWork.cs
+++ SmartMngShop.Customer/Domain/Models/DateCustomerWork.cs
@@ -10,33 +10,32 @@
 
         public int Vacation { get; set; }
 
-        private List<DateCustomerWork> CalculateVacation( int startVacation, int daysSinceStop, List<DateCustomerWork> customerWork, int minWorkingDays)
+        private async Task<List<DateCustomerWork>> CalculateVacation(int startVacation, int daysSinceStop, List<DateCustomerWork> customerWork, int minWorkingDays)
         {
             int vacationThreshold = startVacation;
             DateTime today = DateTime.UtcNow;
 
-            var usersWithVacationLeft = customerWork
-        .Select(x =>
-        {
-            
-            var daysWorked = (today - x.DateTimeStartPrgoress.ToDateTime(TimeOnly.MinValue)).Days;
-
-           
-            var vacationDays = daysWorked >= 28 ? (daysWorked / 28) * 5 : 0;
-
-           
-            return new DateCustomerWork
+            var usersWithVacationLeft = await Task.Run(() =>
             {
-                Id = x.Id,
-                Name = x.Name,
-                JobTitle = x.JobTitle,
-                DateTimeStartPrgoress = x.DateTimeStartPrgoress,
-                DateTimeStopPrgoress = x.DateTimeStopPrgoress,
-                Vacation = vacationDays
-            };
-        })
-        .Where(x => x.Vacation > startVacation)
-        .ToList();
+                return customerWork
+                    .Select(x =>
+                    {
+                        var daysWorked = (today - x.DateTimeStartPrgoress.ToDateTime(TimeOnly.MinValue)).Days;
+                        var vacationDays = daysWorked >= 28 ? (daysWorked / 28) * 5 : 0;
+
+                        return new DateCustomerWork
+                        {
+                            Id = x.Id,
+                            Name = x.Name,
+                            JobTitle = x.JobTitle,
+                            DateTimeStartPrgoress = x.DateTimeStartPrgoress,
+                            DateTimeStopPrgoress = x.DateTimeStopPrgoress,
+                            Vacation = vacationDays
+                        };
+                    })
+                    .Where(x => x.Vacation > vacationThreshold)
+                    .ToList();
+            });
 
             return usersWithVacationLeft;
         }
