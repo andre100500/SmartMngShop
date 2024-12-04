96404c7c9adaa9b656cef6f8f0657c7d4e29c791
andre100500 <andre100500@gmail.com>
2024-12-05 01:24:04 +0200

    add context for customer users

---
+++ SmartMngShop.Customer/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartMngShop.Customer.Domain.Models;
+
+namespace SmartMngShop.Customer.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<DateCustomerWork> works { get; set; }
+    }
+}
